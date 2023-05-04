853aa3e81be274e04feef993d1592dc4f30ffaef
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-05-04 11:59:10 -0300

    iebaaaaaaa commit

--- conversordetemperatura.java
+++ conversordetemperatura.java
@@ -1,2 +1,31 @@
-//Saída
-            System.out.println(n1 + " * " + n2 + " = " + total);
+ackage pct;
+
+import java.util.Scanner;
+
+public class Celfah {
+	public class Conversor {
+		public static void main(String[] args) {
+			//Declaração de variáveis
+			double cel, fah;
+			
+			//Criação instância objeto de entrada
+			Scanner entrada = new Scanner(System.in);
+			
+			//Apresentação
+			System.out.println("\n\t\t\t ---Conversor de Cel para Fah ---\n");
+			
+			//Entrada
+			System.out.println("Informe cel: ");
+			cel = entrada.nextDouble();
+			
+			//Processamento
+			fah =  9 * cel / 5 + 32;
+			
+			//Saída
+			System.out.println("\n\n" + 9 + " * " + cel + " / " + 5 + " + " + 32 + " = " + fah + "\n");
+			
+
+		}
+		
+	}
+}
